7ef17d7cde481ef79b85bdf2368d5d941e4c8ee1
amubarak <amubarak@wooppay.com>
2019-03-14 16:54:56 +0600

    m

---
+++ src/domain/messages/uz/exceptions.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+return [
+	
+	'title' => 'Хато',
+	
+	'yii2lab\\init\\domain\\exceptions\\NotInitApplicationException' => [
+		//'title' => '',
+		'name' => 'Илова инициализация қилинмаган',
+		'message' => 'Иловани инициализация қилинг',
+	],
+
+    'yii2lab\\domain\\exceptions\\UnprocessableEntityHttpException' => [
+        //'title' => '',
+        'name' => 'Нотўғри маълумотлар берилди',
+    ],
+
+];
